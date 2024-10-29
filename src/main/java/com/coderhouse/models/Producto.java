f7f6fac39c2b7b86db2db5dc502dcdaad73d9300
WonRax666 <nicolasradziwon@gmail.com>
2024-10-29 12:01:17 -0300

    1.0.0

---
+++ src/main/java/com/coderhouse/models/Producto.java
@@ -0,0 +1,82 @@
+package com.coderhouse.models;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Productos")
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	private String descripcion;
+	private Double precioUnitario;
+	private Integer stock;
+	
+	@OneToMany(mappedBy = "producto")
+    private List<DetalleVenta> detallesVenta;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(Double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public List<DetalleVenta> getDetallesVenta() {
+		return detallesVenta;
+	}
+
+	public void setDetallesVenta(List<DetalleVenta> detallesVenta) {
+		this.detallesVenta = detallesVenta;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precioUnitario="
+				+ precioUnitario + ", stock=" + stock + ", detallesVenta=" + detallesVenta + "]";
+	}
+	
+	
+}
